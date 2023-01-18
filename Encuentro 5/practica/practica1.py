0269e1b6d933de6b7b32aa58bd8964ca0046f5f8
milepeletay <milepeletaymartinez@gmail.com>
2023-01-18 20:25:23 -0300

    encuentro 5

---
+++ Encuentro 5/practica/practica1.py
@@ -0,0 +1,14 @@
+# kjdsfkjgsjfsjdgfjsgfj
+#hola soy un comentario
+
+edad_milena = 25 #tipo de dato numerico entero guardado en la variable edad_milena
+
+print(edad_milena)
+
+edad_milena = "25 años" #tipo de dato alfanumerico guardado en la variable edad_milena
+
+print(edad_milena)
+
+edad_milena = input("Hola milena, ingresa tu edad: ") #capturamos un dato por la terminal
+
+print("chau mile, tenes " , edad_milena , " años")
